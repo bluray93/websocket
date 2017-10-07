0b227fe4659cc36e96328da2b0a6d0228317a021
bluray93 <bluray219@gmail.com>
2017-10-07 11:40:54 +0100

    Some fixtures

--- server.cpp
+++ server.cpp
@@ -13,36 +13,47 @@
 #include <pthread.h>
 using namespace std;
 
-void *task1(void *);
-
 static int connFd;
 
-int main(int argc, char* argv[]){
+void *task1 (void *dummyPt){
+    cout << "Thread No: " << pthread_self() << endl;
+    char test[300];
+    bzero(test, 301);
+    while(true) {
+        bzero(test, 301);
+        read(connFd, test, 300);
+        string tester (test);
+        cout << tester << endl;
+        if(tester == "exit") break;
+    }
+    cout << "\nClosing thread and conn" << endl;
+    close(connFd);
+    pthread_exit(NULL);
+}
 
+int main(int argc, char* argv[]){
     int pId, portNo, listenFd;
     socklen_t len; //store size of the address
-    bool loop = false;
     struct sockaddr_in svrAdd, clntAdd;
-    pthread_t threadA[3];
-
+    pthread_t threadA;
     if (argc < 2){
         cerr << "Syntam : ./server <port>" << endl;
         return 0;
     }
-
     portNo = atoi(argv[1]);
-
     if((portNo > 65535) || (portNo < 2000)){
         cerr << "Please enter a port number between 2000 - 65535" << endl;
         return 0;
     }
-
     //create socket
     listenFd = socket(AF_INET, SOCK_STREAM, 0);
-
+    int enable = 1;
+    if (setsockopt(listenFd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0){
+      cerr << "setsockopt" << endl;
+    }
     if(listenFd < 0){
-        cerr << "Cannot open socket" << endl;
-        return 0;
+      cerr << "Cannot open socket" << endl;
+      return 0;
     }
 
     bzero((char*) &svrAdd, sizeof(svrAdd));
@@ -52,51 +63,30 @@ int main(int argc, char* argv[]){
 
     //bind socket
     if(bind(listenFd, (struct sockaddr *)&svrAdd, sizeof(svrAdd)) < 0){
-        cerr << "Cannot bind" << endl;
+      cerr << "Cannot bind" << endl;
+      return 0;
+    }
+
+    if (listen(listenFd, 5)<0){
+      perror("listen");
+      exit(EXIT_FAILURE);
+    }
+    len = sizeof(clntAdd);
+    cout << "Listening" << endl;
+    //this is where client connects. svr will hang in this mode until client conn
+    if((connFd = accept(listenFd, (struct sockaddr *)&clntAdd, &len))<0){
+      perror("accept");
+      exit(EXIT_FAILURE);
+    }
+    if (connFd < 0){
+        cerr << "Cannot accept connection" << endl;
         return 0;
     }
-
-    listen(listenFd, 5);
-    len = sizeof(clntAdd);
-    int noThread = 0;
-
-    while (noThread < 3){
-        cout << "Listening" << endl;
-
-        //this is where client connects. svr will hang in this mode until client conn
-        connFd = accept(listenFd, (struct sockaddr *)&clntAdd, &len);
-
-        if (connFd < 0){
-            cerr << "Cannot accept connection" << endl;
-            return 0;
-        }
-        else{
-            cout << "Connection successful" << endl;
-        }
-
-        pthread_create(&threadA[noThread], NULL, task1, NULL);
-
-        noThread++;
+    else{
+        cout << "Connection successful" << endl;
     }
 
-    for(int i = 0; i < 3; i++){
-        pthread_join(threadA[i], NULL);
-    }
-
-}
-
-void *task1 (void *dummyPt){
-    cout << "Thread No: " << pthread_self() << endl;
-    char test[300];
-    bzero(test, 301);
-    bool loop = false;
-    while(!loop) {
-        bzero(test, 301);
-        read(connFd, test, 300);
-        string tester (test);
-        cout << tester << endl;
-        if(tester == "exit") break;
-    }
-    cout << "\nClosing thread and conn" << endl;
-    close(connFd);
+    pthread_create(&threadA, NULL, task1, NULL);
+    pthread_join(threadA, NULL);
+    return 0;
 }
